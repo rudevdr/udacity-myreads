eada31884ec0b918f197a20237cd112fc271e233
rudevdr <gudayschief@gmail.com>
2018-11-28 01:38:10 +0530

    Updates

---
+++ precache-manifest.3949973baff49755c5599b1a0a897348.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/udacity-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/udacity-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "5fc2c16637d9972bdf2838c4555c00d5",
+    "url": "/udacity-myreads/static/media/add.5fc2c166.svg"
+  },
+  {
+    "revision": "94a001f5414cecc913be",
+    "url": "/udacity-myreads/static/js/runtime~main.94a001f5.js"
+  },
+  {
+    "revision": "d4cfd3a810393d0b0f4f",
+    "url": "/udacity-myreads/static/js/main.d4cfd3a8.chunk.js"
+  },
+  {
+    "revision": "f195e7021685edcc4072",
+    "url": "/udacity-myreads/static/js/1.f195e702.chunk.js"
+  },
+  {
+    "revision": "d4cfd3a810393d0b0f4f",
+    "url": "/udacity-myreads/static/css/main.e95f06b4.chunk.css"
+  },
+  {
+    "revision": "d729ef2bdde799bc9dd270a1cfd61098",
+    "url": "/udacity-myreads/index.html"
+  }
+];

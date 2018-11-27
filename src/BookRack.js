75a5a393a750c35fad61e38c5139fee8828c681f
rudevdr <gudayschief@gmail.com>
2018-11-27 21:36:16 +0530

    Made changes for v0.1

--- src/BookRack.js
+++ src/BookRack.js
@@ -1,83 +1,67 @@
 import React, {Component} from 'react'
 import './App.css'
+import {Link} from "react-router-dom";
+
 import * as BooksAPI from "./BooksAPI";
+import BookShelf from "./BookShelf";
 
 class BookRack extends Component {
-
-	constructor(){
-		super()
+	constructor(props){
+		super(props);
 		this.state = {
-			/**
-			 * TODO: Instead of using this state variable to keep track of which page
-			 * we're on, use the URL in the browser's address bar. This will ensure that
-			 * users can use the browser's back and forward buttons to navigate between
-			 * pages, as well as provide a good URL they can bookmark and share.
-			 */
-			//		showSearchPage: false,
 			books : []
-		}}
+		};
+		this.updateBookShelf = this.updateBookShelf.bind(this);
+	}
+
 
 	getBooksByShelf(shelf) {
 		return this.state.books.filter(book => book.shelf === shelf);
 	}
 
-    componentWillMount() {
+	updateBookShelf(book, shelf) {
+		BooksAPI.update(book, shelf).then((response) => {
+			this.fetchAllBooks();
+		});
+	}
+
+	fetchAllBooks(){
         BooksAPI.getAll().then((books) => {
             this.setState({books: books});
         });
+
+	}
+
+    componentWillMount() {
+		this.fetchAllBooks();
     }
 
 
 	render(){
 		return (
-			<div className="app">
-				<div className="list-books">
-					<div className="list-books-title">
-						<h1>MyReads</h1>
-					</div>
-					<div className="list-books-content">
-						<div>
-							<BookShelf name="Currently Reading" books={this.getBooksByShelf('currentlyReading')}/>
-							<BookShelf name="Want To Read" books={this.getBooksByShelf('wantToRead')}/>
-							<BookShelf name="Read" books={this.getBooksByShelf('read')}/>
+				<div className="app">
+					<div className="list-books">
+						<div className="list-books-title">
+							<h1>MyReads</h1>
 						</div>
-					</div>
-					<div className="open-search">
-						<button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-					</div>
-				</div>
-			</div>
-		)}
-}
-
-const BookShelf = (props) => (
-	<div className="bookshelf">
-		<h2 className="bookshelf-title">{props.name}</h2>
-		<div className="bookshelf-books">
-			<ol className="books-grid">
-				{props.books.map((book) => (
-				<li>
-					<div className="book">
-						<div className="book-top">
-							<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")`}}></div>
-							<div className="book-shelf-changer">
-								<select>
-									<option value="move" disabled>Move to...</option>
-									<option value="currentlyReading">Currently Reading</option>
-									<option value="wantToRead">Want to Read</option>
-									<option value="read">Read</option>
-									<option value="none">None</option>
-								</select>
+						<div className="list-books-content">
+							<div>
+								<BookShelf name="Currently Reading" books={this.getBooksByShelf('currentlyReading')} updateBookShelf={this.updateBookShelf} />
+								<BookShelf name="Want To Read" books={this.getBooksByShelf('wantToRead')} updateBookShelf={this.updateBookShelf} />
+								<BookShelf name="Read" books={this.getBooksByShelf('read')} updateBookShelf={this.updateBookShelf}/>
 							</div>
 						</div>
-						<div className="book-title">{book.title}</div>
-						<div className="book-authors">{book.author}</div>
+						<div className="open-search">
+							<div className="open-search">
+								<Link to="/search">
+									<button >Add a book</button>
+								</Link>
+							</div>
+						</div>
 					</div>
-				</li>
-				))}
-			</ol>
-		</div>
-	</div>
-	)
+				</div>
+		)
+	}
+}
 
 export default BookRack

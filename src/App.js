13bd1ca38b478184537bd98f8e2414d1ae7878c4
rudevdr <gudayschief@gmail.com>
2018-11-27 21:34:58 +0530

    Modified to add react-routers

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react'
-import BookRack from './BookRack.js'
+import {Route} from "react-router-dom";
+import BookRack from './BookRack'
+import BookSearch from './BookSearch'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
-  render() {
-    return <BookRack/>
-  }
+	render() {
+		return (
+			<div className="app">
+				<Route exact path="/" render={() => <BookRack/>} />
+				<Route
+					path="/search"
+					render={() =>
+					<BookSearch />
+					}/>
+			</div>
+		)
+	}
 }
 
 export default BooksApp
